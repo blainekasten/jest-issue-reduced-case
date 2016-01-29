5a44e524399102d39ca494f0e11a1703320d968b
blainekasten <blainekasten@gmail.com>
2016-01-29 15:14:23 -0600

    initial commit

---
+++ src/__tests__/Fails.js
@@ -0,0 +1,7 @@
+const IncludesGulpUtil = require('../IncludesGulpUtil');
+
+describe('Fails', () => {
+  it('even though this should pass', () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ src/__tests__/Passes.js
@@ -0,0 +1,9 @@
+
+// passes because this isn't included
+//const IncludesGulpUtil = require('../IncludesGulpUtil');
+
+describe('Passes', () => {
+  it('even though this should pass', () => {
+    expect(1).toBe(1);
+  });
+});
